41460b83021db16b8c02cc7e8fcb05dba33e5a12
mixt <evakid1106@gmail.com>
2015-03-02 20:25:02 +0800

    solve binary tree inorder traversal

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    List<Integer> traversal = new ArrayList();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(null != root) {
+            traversal = inorderTraversal(root.left);
+            traversal.add(root.val);
+            traversal = inorderTraversal(root.right);
+        }
+        return traversal;
+    }
+}
+
+/**
+ * 比较标准的二叉树前序遍历，注意作为返回值的list在函数外定义并初始化
+ */
