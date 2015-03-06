2cb75c6526623bb0c1ac927fb3e54c894464798a
mixt <evakid1106@gmail.com>
2015-03-07 00:06:28 +0800

    Linked List Cycle solution v2

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -11,22 +11,25 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
         if(head == null || head.next == null) {
             return false;
         }
-        else if(head.next == head) {
-            return true;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                return true;
+            }
         }
-        ListNode fhead = head.next;
-        head.next = head;
-        boolean result = hasCycle(fhead);
-        return result;
+        return false;
     }
 }
 
 /**
- * 基本想法是顺序遍历list中的节点，使用某种方法标记已遍历过的节点，直到发现经过一个已被标记的节点，
- * 这时表明list中有cycle。这里的难点是如何在不使用额外内存的条件下在递归的过程中记录哪些节点已经遍历过。
- * 这里用的方法是每经过一个node就将它的next成员指向自己，然后继续遍历下一个node，直到经过某个node发现它的next是它自己，
- * 这时发现有cycle。这里遍历用递归实现。要注意的是无cycle的终止条件，其中head == null容易被忽略。
+ * 这题的思路是设置两个游标，一个每次迭代向后走两步，叫fast，一个每次迭代走一步，叫slow。
+ * 如果fast自己移动到null，则无环。如果fast和slow在某次迭代中指向了同一个节点，则有环。
+ * 据说此题是微软经典面试题。。。。
+ * 之前的一个解法虽然AC，但会破坏这个list，算了，太丢人了。
  */
