30dde683315f8f66c5ccc90621f60db0d056c89e
Yi Fan <evakid1106@hotmail.com>
2015-09-18 00:44:15 +0800

    add

--- zigzag-conversion.java
+++ zigzag-conversion.java
@@ -1,5 +1,8 @@
-public class zigzag-conersion {
+public class Zigzag-concersion {
     public String convert(String s, int numRows) {
+        if(numRows == 1) {
+            return s;
+        }
         char[] arr = s.toCharArray();
         Object[] matrix = new Object[numRows];
         for(int i = 0; i < matrix.length; i++) {
@@ -9,20 +12,22 @@ public class zigzag-conersion {
         int len = arr.length;
         int count = 0;
         for(int i = 0; i < len; i++) {
-            if(!flag) {
-                (ArrayList<Character>)matrix[count].add(arr[i]);
+            if(!flag && count < numRows) {
+                ((ArrayList<Character>)matrix[count]).add(arr[i]);
                 count++;
-                if(count >= numRows) {
-                    flag = true;
-                    count = 1;
+                if(count == numRows) {
+                    if(numRows >= 3) {
+                        flag = true;
+                    }    
+                    count = 0;
                 }
             }
-            else {
-                (ArrayList<Character>)matrix[count].add(arr[i]);
-                count = count + 2;
-                if(count >= numRows) {
-                    flag = false;
+            else if(flag && count < numRows - 2) {
+                ((ArrayList<Character>)matrix[numRows - 2 - count]).add(arr[i]);
+                count++;
+                if(count == numRows - 2) {
                     count = 0;
+                    flag = false;
                 }
             }
         }
@@ -30,6 +35,10 @@ public class zigzag-conersion {
         for(int i = 0; i < numRows; i++) {
             total.addAll((ArrayList<Character>)matrix[i]);
         }
-        return new String(total);
+        StringBuilder result = new StringBuilder();
+        for(Character character : total) {
+            result.append(character);
+        }
+        return result.toString();
     }
 }
